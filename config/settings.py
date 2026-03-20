5593f90f3093ccc8e2899f688e6a62222a8fed57
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-21 00:36:26 +0300

    Установлен пакет для работы с правами доступа, добавлены настройки для него. В приложении users добавлены два новых маршрута

--- config/settings.py
+++ config/settings.py
@@ -43,8 +43,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
+    "rest_framework_simplejwt",
     "django_filters",
+
     "users",
     "lms",
 ]
@@ -136,3 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 from users.views import PaymentListView, UserListView, UserProfileView
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path("", UserListView.as_view(), name="user-list"),
     path("<int:pk>/", UserProfileView.as_view(), name="user-profile"),
     path("payments/", PaymentListView.as_view(), name="payment-list"),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
