f9bd78769ba7cfb3e0736156954cfff585572f9f
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-20 10:59:55 +0300

    Для модели курса добавлен сериализатор Детальной информации с полем вывода количества уроков.

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,14 +4,44 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    """
+    Базовый сериализатор для модели Course.
+    Используется для базового вывода информации о курсе.
+    """
 
     class Meta:
         model = Course
         fields = "__all__"
 
+class CourseDetailSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для детальной информации о курсе.
+    Добавляет вычисляемое поле lessons_count с количеством уроков.
+    """
+
+    # Объявляем поле, значение которого будет вычисляться динамически
+    lessons_count = serializers.SerializerMethodField()
+
+    # Метод для вычисления кол-ва уроков
+    def get_lessons_count(self, instance):
+        # instance - Текущий курс, курс, который сериализуем
+        # instance.lessons - Все уроки этого курса
+        # instance.lessons.count() - Вызываем метод count() для подсчёта количества
+
+        return instance.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ('id', 'title','description', 'lessons_count')
+
 
 class LessonSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Lesson.
+    Используется для вывода информации об уроке.
+    """
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
