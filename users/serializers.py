863d9c523166a794dafeddc4c2284c984a6434bf
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-22 18:50:36 +0300

    Добавлен сериализатор для создания нового пользователя

--- users/serializers.py
+++ users/serializers.py
@@ -87,6 +87,39 @@ class UserPaymentSerializer(serializers.ModelSerializer):
             "payment_method_display",
         ]
 
+class UserCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания нового пользователя"""
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'city', 'avatar', 'password']
+
+        read_only_fields = ['id']
+
+    def validate_email(self, value):
+        """Проверяем, что email не занят"""
+        if User.objects.filter(email=value).exists():
+             raise serializers.ValidationError('Пользователь с таким email уже существует')
+        return value
+
+    def create(self, validated_data):
+        """
+        Создаёт пользователя с хэшированным паролем.
+        """
+
+        # Забираем пароль и email из словаря
+        password = validated_data.pop('password')
+        email = validated_data.pop('email')
+
+        # Создаем пользователя
+        user = User.objects.create_user(
+            email=email,
+            password=password,
+            **validated_data,   # first_name, last_name, phone, city и т.д.
+        )
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
     """
