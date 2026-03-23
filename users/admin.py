9681a33f6728de7deffc6d27b3c810c344ddb2a8
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-23 15:28:14 +0300

    Добавлено отображение групп у пользователей и модели платежей для удобства отладки

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from .models import User, Payments
 
-from users.models import User
-
-
-# Register your models here.
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_filter = ('id', 'email')
+    list_display = ("email", "display_groups", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email",)
+
+    def display_groups(self, obj):
+        """Показывает группы пользователя"""
+        return ", ".join([group.name for group in obj.groups.all()])
+
+    display_groups.short_description = "Группы"
+
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ("user", "payment_date", "payment_amount", "payment_method")
+    list_filter = ("payment_method",)
+    search_fields = ("user__email",)
