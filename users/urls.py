df93e0a4984d0493032da12b6d8e394d4db814d2
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-26 01:55:44 +0300

    Исправлена опечатка модели Подписка. Добавлен маршрут для добавления подписки

--- users/urls.py
+++ users/urls.py
@@ -4,16 +4,24 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 
 from users.apps import UsersConfig
 from users.views import (PaymentCreateAPIView, PaymentListView,
-                         UserCreateAPIView, UserListView, UserProfileView)
+                         UserCreateAPIView, UserListView, UserProfileView, SubscriptionAPIView)
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    # Пользователи
     path("", UserListView.as_view(), name="user-list"),
     path("<int:pk>/", UserProfileView.as_view(), name="user-profile"),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+
+    # Платежи
     path("payments/", PaymentListView.as_view(), name="payment-list"),
     path("payments/create/", PaymentCreateAPIView.as_view(), name="payment-create"),
-    path("register/", UserCreateAPIView.as_view(), name="register"),
+
+    # Подписки (на курсы)
+    path("courses/subscribe/", SubscriptionAPIView.as_view(), name="subscribe"),
+
+    # Авторизация
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
