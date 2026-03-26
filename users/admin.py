5620c2eba56bf28a9fef27f646b698c7e0757466
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-26 03:36:27 +0300

    Добавлено отображение подписки в админке

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Payments, User
+from .models import Payments, User, Subscription
 
 
 @admin.register(User)
@@ -18,6 +18,10 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(Payments)
 class PaymentsAdmin(admin.ModelAdmin):
-    list_display = ("user", "payment_date", "payment_amount", "payment_method")
+    list_display = ("user", "payment_date", "payment_amount", "payment_method", "paid_course", "paid_lesson")
     list_filter = ("payment_method",)
     search_fields = ("user__email",)
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "course", "created_at")
