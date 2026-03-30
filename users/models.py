fb146239707958954f16de07d5a90476f79bc92f
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-30 12:49:37 +0300

    Добавлены поля для работы Stripe(оплата).  Добавлена оплата картой и статус платежа (для дальнейшей реализации)

--- users/models.py
+++ users/models.py
@@ -85,6 +85,7 @@ class Payments(models.Model):
     PAYMENT_METHODS = [
         ("cash", "Наличные"),
         ("transfer", "Перевод на счет"),
+        ("card", "Банковская карта")
     ]
 
     user = models.ForeignKey(
@@ -119,19 +120,34 @@ class Payments(models.Model):
         default="cash",
         verbose_name="Способ оплаты",
     )
-    session_id = models.CharField(
+    # Поля для Stripe
+    stripe_product_id = models.CharField(
         max_length=255,
         blank=True,
         null=True,
-        verbose_name="Id сессии",
-        help_text="Укажите Id сессии"
+        verbose_name="Stripe Product ID"
     )
-    link = models.URLField(
-        max_length=400,
+    stripe_price_id = models.CharField(
+        max_length=255,
         blank=True,
         null=True,
-        verbose_name="Сссылка на оплау",
-        help_text="Укажите ссылку на оплату"
+        verbose_name="Stripe Price ID"
+    )
+    stripe_session_id = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="Stripe Session ID"
+    )
+    payment_url = models.URLField(
+        max_length=500,
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на оплату"
+    )
+    is_paid = models.BooleanField(
+        default=False,
+        verbose_name="Оплачено"
     )
 
     class Meta:
