dede92084656bd39256abf7f9b6c47256269d73f
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-03-30 14:31:28 +0300

    Маршруты для документации с использованием spectacular

--- config/urls.py
+++ config/urls.py
@@ -41,9 +41,6 @@ urlpatterns = [
     path("api/lms/", include("lms.urls", namespace="lms")),
     path("api/users/", include("users.urls", namespace="users")),
 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
