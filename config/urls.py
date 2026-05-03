79237342b6da8b8eb895b74de8e91f6ccc14db27
Vika <vika.davlyatshina@mail.ru>
2026-05-03 12:49:34 +0200

    Удалены нстройки"drf_yasg". Для документации используется только drf_spectacular, настройки указаны в settings

--- config/urls.py
+++ config/urls.py
@@ -19,22 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="LMS API",
-        default_version="v1",
-        description="API для управления курсами и уроками",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
